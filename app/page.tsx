454e7b034475cadd1e0bda045143a6e7566e5a23
shxxkir <128359891+shxxkir@users.noreply.github.com>
2025-05-28 11:13:07 +0530

    feat: remove AchievementsSection import and comment out its usage in Home component

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,6 @@ import TechnologyStackSection from "@/components/technology-stack-section";
 import MilestonesSection from "@/components/milestones-section";
 import DownloadsSection from "@/components/downloads-section";
 import AboutUsSection from "@/components/about-us-section";
-import AchievementsSection from "@/components/achievements-section";
 import ContactUsSection from "@/components/contact-us-section";
 import FinalCtaSection from "@/components/final-cta-section";
 
@@ -22,7 +21,6 @@ export default function Home() {
         <MilestonesSection />
         <DownloadsSection />
         <AboutUsSection />
-        {/* <AchievementsSection /> */}
         <ContactUsSection />
         <FinalCtaSection />
       </main>
